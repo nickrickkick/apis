8d921d836c25ccd4c0b4f3f198564f5f23730ad4
Michael Nowlin <michaelnowlin5@gmail.com>
2022-04-05 22:30:42 -0600

    finished

---
+++ swapi/main.js
@@ -0,0 +1,26 @@
+const API_BASE_URL = 'https://swapi.dev/api/planets?search=alderaan';
+const butChange = document.querySelector('button');
+
+const showResidents = (event) => {
+    console.log("button clicked");
+    axios.get(API_BASE_URL).then((res) => {
+        const planet = res.data.results;
+        console.log(planet);
+        const residents = planet.map((names) => names.residents);
+        //its a double array or 2d array
+        console.log(residents[0][1]);
+        for(let i = 0; i < residents[0].length; i++){
+            console.log(residents[0][i]);
+            axios.get(residents[0][i]).then((res) => {
+                let person = res.data.name;
+                console.log(person);
+                var people = document.createElement("h2");
+                people.innerHTML = person;
+                document.body.appendChild(people);
+            })
+        }
+        
+    })
+}
+
+butChange.addEventListener("click", showResidents);
